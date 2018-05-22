47a2dbd448ec9a462d7ee6b58511580c87ea715f
dinara0708 <aikenova01@gmail.com>
2018-05-22 19:07:45 +0600

    -

--- ConsoleApplication1/Practice.cpp
+++ ConsoleApplication1/Practice.cpp
@@ -1,2 +1,19 @@
-///
-int =0
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int n = 0;
+	printf("Введите номер задания:\t");
+	scanf("%d", &n);
+
+	if (n == 1) {
+
+
+
+	}
+
+}
